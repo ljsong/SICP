cfa9b3e0308d6b1b9995a2d14c781bd3baca6ce7
AllenSong <logensong@gmail.com>
2015-07-14 00:27:17 +0800

    add solution for section 3, chapter 1

---
+++ sicp_ch_01_03.rkt
@@ -0,0 +1,60 @@
+#lang racket
+
+(define (cube x)
+  (* x x x))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n)
+  (+ n 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
+
+(define (identity x) x)
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 100000))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(integral cube 0 1 0.001)
+
+(define (even? x)
+  (= (remainder x 2) 0))
+
+(define (simpson-integral f a b n)
+  (define h (/ (- b a) n))
+  (define (simpson-term x)
+    (define k (/ (- x a) h))
+    (cond ((or (= k 0) (= k n)) (f x))
+          ((even? k) (* 2 (f x)))
+          (else (* 4 (f x)))))
+  (define (simpson-next x)
+    (+ x h))
+    
+  (* (/ h 3) (sum simpson-term a simpson-next b)))
+
+(simpson-integral cube 0 1 100)
+  
+  
