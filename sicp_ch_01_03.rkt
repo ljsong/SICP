5e455ba43afee83f766837611eafeae6105cb781
songlongjun@pset.suntec.net <songlongjun@pset.suntec.net>
2015-07-14 14:44:51 +0800

    add solution for exercise 1.30

--- sicp_ch_01_03.rkt
+++ sicp_ch_01_03.rkt
@@ -3,11 +3,21 @@
 (define (cube x)
   (* x x x))
 
-(define (sum term a next b)
+; This is a recursive version
+#|(define (sum term a next b)
   (if (> a b)
       0
       (+ (term a)
          (sum term (next a) next b))))
+|#
+
+; iter version
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
 
 (define (inc n)
   (+ n 1))
