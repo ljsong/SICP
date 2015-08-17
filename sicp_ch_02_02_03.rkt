000618424d4384972615de8836de203817963c45
songlongjun <songlongjun@pset.suntec.net>
2015-08-17 15:32:11 +0800

    add solution for exercise of subsection 3

--- sicp_ch_02_02_03.rkt
+++ sicp_ch_02_02_03.rkt
@@ -65,12 +65,37 @@
 (append (list 1 2 3 4) (list 5 6 7 8))
 
 (define (length-test sequence)
-  (accumulate (lambda (x y) 
+  (accumulate (lambda (x y)
                 (cond ((null? x) y)
-                      ((not (pair? x)) (+ 1  y))
-                      (else
-                       (+ x
-                          y)))) 0 sequence))
-; 此时的x和y已经是(accumulate op initial sequence)
+                      (else (+ 1 y)))) 0 sequence))
+; 此时的x和y已经是(accumulate op initial sequence),注意根据op操作的不同
+; (accumulate op initial sequence)的类型也不同，可能是一个数，也可能是一个序列等等
 
-(length-test (list 1 (list 2 (list 3)) (list 4 5)))
+(length-test (list (list 2 3)))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff
+                   (* x higher-terms)))
+              0
+              coefficient-sequence))
+;higher-terms 此时为(accumulate op initial (cdr sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (sub-tree)
+                         (if (pair? sub-tree)
+                             (count-leaves sub-tree)
+                             1)) t)))
+
+(count-leaves (list (list 1 2) 3 (list 4 (list 5))))
+
+(define (accumulate-n op initial seqs)
+  (if (null? (car seqs))
+      null
+      (cons (accumulate op initial (map car seqs))
+            (accumulate-n op initial (map cdr seqs)))))
+
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(accumulate-n + 0 s)
