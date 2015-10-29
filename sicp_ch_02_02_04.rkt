30aa920d5f9ebcebb331876027abb8ead908ed1c
songlongjun <songlongjun@pset.suntec.net>
2015-10-29 17:20:21 +0800

    finish sub chapter 2

--- sicp_ch_02_02_04.rkt
+++ sicp_ch_02_02_04.rkt
@@ -44,7 +44,7 @@
       null
       (append (list m) (enumerate-interval (+ m 1) n))))
 
-(define (prime-sum-pairs n)
+#|(define (prime-sum-pairs n)
   (map make-pair-sum
        (filter prime-sum?
                (flatmap
@@ -52,6 +52,19 @@
                   (map (lambda (j) (list i j))
                        (enumerate-interval 1 (- i 1))))
                 (enumerate-interval 1 n)))))
+|#
+
+(define (unique-pairs n)
+  (map (lambda (i)
+         (map (lambda (j)
+                (list i j))
+              (enumerate-interval 1 (- i 1))))
+       (enumerate-interval 1 n)))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (accumulate
+                           append null (unique-pairs n)))))
 
 (define (test x)
  (map (lambda (i)
@@ -76,3 +89,53 @@
                s)))
 
 (permutations (list 1 2 3 4))
+
+(unique-pairs 5)
+
+(define (triple-sum n s)
+  (define all-seqs (map (lambda (i)
+         (map (lambda (j)
+                     (if (and (not (= j i))
+                                (>= (- s i j) 1)
+                                (<= (- s i j) n)
+                                (not (= (- s i j) i))
+                                (not (= (- s i j) j)))
+                       (list i j (- s i j))
+                       null))
+              (enumerate-interval 1 n)))
+       (enumerate-interval 1 n)))
+  (filter (lambda (seq)
+            (not (null? seq)))
+          (accumulate append null all-seqs)))
+                     
+
+(triple-sum 6 10)
+
+(define empty-board null)
+
+(define (safe? k positions)
+  (define pos (car (reverse positions)))
+  (null? (filter (lambda (seq) (or (= (car pos) (car seq))
+                                     (= (abs (- (cadr pos) (car pos)))
+                                        (abs (- (cadr seq) (car seq))))))
+                 (cdr (reverse positions)))))
+
+(define (queens board-size)
+  (define (adjoin-position new-row k rest-of-queens)
+    (append rest-of-queens (list (list new-row k))))
+  
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  
+  (queen-cols board-size))
+
+(queens 8)
