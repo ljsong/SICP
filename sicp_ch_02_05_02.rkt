df4cde0623b2632de19e85cf8803b751746246a7
songlongjun <songlongjun@pset.suntec.net>
2016-03-15 22:43:29 +0800

    modify some errors

--- sicp_ch_02_05_02.rkt
+++ sicp_ch_02_05_02.rkt
@@ -36,18 +36,18 @@
     (cons variable term-list))
   (define (variable p) (car p))
   (define (term-list p) (cdr p))
-
+  
   (define (the-empty-termlist) '())
   (define (first-term term-list) (car term-list))
   (define (rest-terms term-list) (cdr term-list))
   (define (empty-termlist? term-list) (null? term-list))
-
+  
   (define (make-term order coeff) (list order coeff))
   (define (order term) (car term))
   (define (coeff term) (cadr term))
   
   (define (variable? x) (symbol? x))
-
+  
   (define (add-terms L1 L2)
     (cond ((empty-termlist? L1) L2)
           ((empty-termlist? L2) L1)
@@ -56,43 +56,65 @@
              (cond ((> (order t1) (order t2))
                     (adjoin-term
                      t1 (add-terms (rest-terms L1) L2)))
-                   ((< (order t1) (order t2)
-                       (adjoin-term
-                        t2 (add-terms L1) (rest-terms L2))))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2 (add-terms L1 (rest-terms L2))))
                    (else
                     (adjoin-term
                      (make-term (order t1)
                                 (add (coeff t1) (coeff t2)))
                      (add-terms (rest-terms L1)
                                 (rest-terms L2)))))))))
-
+  
   (define (sub-terms L1 L2)
-    (add-terms L1 (neg L2)))
-
+    (add-terms L1 (neg-terms L2)))
+  
   (define (mul-terms L1 L2)
     (if (empty-termlist? L1)
         (the-empty-termlist)
         (add-terms (mul-term-by-all-terms (first-term L1) L2)
                    (mul-terms (rest-terms L1) L2))))
-
+  
   (define (mul-term-by-all-terms t1 L)
     (if (empty-termlist? L)
         (the-empty-termlist)
         (let ((t2 (first-term L)))
-          (adjoin-term (+ (order t1) (order t2))
-                       (mul (coeff t1) (coeff t2)))
-          (mul-term-by-all-terms t1 (rest-terms L)))))
-
+          (adjoin-term
+           (make-term (+ (order t1) (order t2))
+                      (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+  
+  (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist) (the-empty-termlist))
+        (let ((t1 (first-term L1))
+              (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist) L1)
+              (let ((new-c (div (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((rest-of-result 
+                      (div-terms (sub-terms L1 (mul-terms (list (make-term new-o new-c)) L2)) L2)))
+                  (display rest-of-result)
+                  (cons (add-terms (make-term new-o new-c) (car rest-of-result)) (cdr rest-of-result))
+                  ))))))
+  
+  (define (div-poly p1 p2)
+    (div-terms (term-list p1) (term-list p2)))
+  
   (define (same-variable? v1 v2)
     (and (variable? v1) (variable? v2) (eq? v1 v2)))
-
+  
+  (define (neg-terms termlist)
+    (if (empty-termlist? termlist)
+        termlist
+        (let ((t1 (first-term termlist)))
+          (let ((neg-term (make-term (order t1) (- 0 (coeff t1)))))
+            (adjoin-term neg-term (neg-terms (rest-terms termlist)))))))
+  
   (define (neg p)
-    (let ((tlist (term-list p)))
-      (let ((t1 (first-term tlist))
-            (rlist (rest-terms tlist)))
-        (let ((neg-term (make-term (- 0 (order t1)) (coeff t1))))
-          (make-poly (variable p) (add-terms neg-term (neg rlist)))))))
-
+    (make-poly (variable p) (neg-terms (term-list p))))
+  
   (define (add-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
         (make-poly (variable p1)
@@ -100,7 +122,7 @@
                               (term-list p2)))
         (error "Polys not in same ver -- ADD-POLY"
                (list p1 p2))))
-
+  
   (define (mul-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
         (make-poly (variable p1)
@@ -108,26 +130,31 @@
                               (term-list p2)))
         (error "Polys not in same var -- MUL-POLY"
                (list p1 p2))))
-
+  
   (define (adjoin-term term term-list)
-    (if (=zero? (coeff term))
+    (if (= (coeff term) 0)
         term-list
         (cons term term-list)))
-
+  
   (define (=zero? p)
-    (let ((tlist (term-list p)))
-      (and (= (order (first-term tlist)) 0) (=zero? (rest-terms tlist)))))
-   
+    (define (=zero? termlist)
+      (if (empty-termlist? termlist)
+          #t
+          (and (= (coeff (first-term termlist)) 0) (=zero? (rest-terms termlist)))))
+    (=zero? (term-list p)))
+  
   (define (tag p) (attach-tag 'polynomial p))
   (put '(polynomial polynomial) 'add
        (lambda (p1 p2) (tag (add-poly p1 p2))))
   (put '(polynomial polynomial) 'mul
        (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put '(polynomial polynomial) 'div
+       (lambda (p1 p2) (tag (div-poly p1 p2))))
   (put 'polynomial 'make
        (lambda (var terms) (tag (make-poly var terms))))
-  (put 'polynomial '=zero?
+  (put '(polynomial) '=zero?
        (lambda (p) (tag (=zero? p))))
-  (put 'polynomial 'neg
+  (put '(polynomial) 'neg
        (lambda (p) (tag (neg p))))
   'done)
 
@@ -141,7 +168,17 @@
 (define (neg p)
   (apply-generic 'neg p))
 
-(define p (make-polynomial 'x '((2 1) (3 2) (63))))
+(define (=zero? p)
+  (apply-generic '=zero? p))
+
+(define (div-polynomial p1 p2)
+  (apply-generic 'div p1 p2))
+
+(define p (make-polynomial 'x '((2 1) (3 2) (6 3))))
 p
 (neg p)
-(add-polynomial p (neg p))
+(define q (add-polynomial p (neg p)))
+(define a (make-polynomial 'x '((5 1) (0 -1))))
+(define b (make-polynomial 'x '((2 1) (0 -1))))
+
+(div-polynomial a b)
