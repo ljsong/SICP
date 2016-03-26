2a4d60c21505ee107e2723191a70959838d1cbed
AllenSong <logensong@gmail.com>
2016-03-26 22:54:56 +0800

    implementation for queue

---
+++ sicp_ch_03_03_02.rkt
@@ -0,0 +1,55 @@
+#lang racket
+(require scheme/mpair)
+
+(define (front-ptr queue) (mcar queue))
+(define (rear-ptr queue) (mcdr queue))
+(define (set-front-ptr! queue item) (set-mcar! queue item))
+(define (set-rear-ptr! queue item) (set-mcdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (mcons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (mcar (front-queue queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (mcons item '())))
+    (if (empty-queue? queue)
+        (begin
+          (set-front-ptr! queue new-pair)
+          (set-rear-ptr! queue new-pair)
+          queue)
+        (begin ;设置分为两步，第一步将new-pair链接到rear-ptr的后面，第二步更新rear-ptr
+          (set-mcdr! (rear-ptr queue) new-pair)
+          (set-rear-ptr! queue new-pair)
+          queue))))
+
+(define (delete-queue! queue)
+  (if (empty-queue? queue)
+      (error "DELETE! called with an empty queue" queue)
+      (begin
+        (set-front-ptr! queue (mcdr (front-ptr queue)))
+        queue)))
+
+(define (print-queue queue)
+  (define (print-item item)
+    (if (null? item)
+        (display "")
+        (begin
+          (display (mcar item))
+          (display " ")
+          (print-item (mcdr item)))))
+  (display "(")
+  (print-item (front-ptr queue))
+  (display ")\n"))
+
+(define q1 (make-queue))
+(insert-queue! q1 'a)
+(print-queue q1)
+(insert-queue! q1 'b)
+(print-queue q1)
+(delete-queue! q1)
+(print-queue q1)
+(delete-queue! q1)
+(print-queue q1)
