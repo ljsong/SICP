1889a6fc34ad9f4de99152ac0d416cddd0bda464
songlongjun <songlongjun@pset.suntec.net>
2016-03-30 15:16:59 +0800

    finish section 3.3.3

--- sicp_ch_03_03_03.rkt
+++ sicp_ch_03_03_03.rkt
@@ -1,79 +1,106 @@
 #lang racket
+(require scheme/mpair)
 
-(define (lookup key table)
-  (let ((record (assoc key (cdr table))))   ; (car table) 表头的哑元
-    (if record
-        (cdr record)
-        false)))
+(define (mcddr pair)
+  (mcdr (mcdr pair)))
+
+(define (mcadr pair)
+  (mcar (mcdr pair)))
+
+(define (mcaar pair)
+  (mcar (mcar pair)))
 
 (define (assoc key records)
   (cond ((null? records) false)
         ((equal? key (caar records)) (car records))  ; (car records)是指向第一个元素的序对
         (else (assoc key (cdr records)))))           ; (caar records)是key, (cadr records)是值
 
-(define (insert! key value table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (set-cdr! record value)
-        (set-cdr! table
-                  (cons (cons key value) (cdr table)))))
-  'OK)
-
-(define (make-table)
-  (list '*table*))
-
-(define (lookup key-1 key-2 table)
-  (let ((subtable (assoc key-1 (cdr table))))
-    (if subtable
-        (let ((record (assoc key-2 (cdr subtable))))
-          (if record
-              (cdr record)
-              false))
-        false)))
-
-(define (insert! key-1 key-2 value table)
-  (let ((subtable (assoc key-1 (cdr table))))
-    (if subtable
-        (let ((record (assoc key-2 (cdr subtable))))
-          (if record
-              (set-cdr! record value)
-              (set-cdr! subtable (cons (cons key-2 value)
-                                       (cdr subtable)))))
-        (set-cdr! table
-                  (cons (list key-1
-                              (cons key-2 value))
-                        (cdr table))))))
-
 (define (make-table)
   (let ((local-table (list '*table*)))
     (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
+      (let ((subtable (assoc key-1 (mcdr local-table))))
         (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
+            (let ((record (assoc key-2 (mcdr subtable))))
               (if record
-                  (cdr record)
+                  (mcdr record)
                   false))
             false)))
     (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
+      (let ((subtable (assoc key-1 (mcdr local-table))))
         (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
+            (let ((record (assoc key-2 (mcdr subtable))))
               (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            (set-cdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (cdr local-table)))))
-        'OK)
+                  (set-mcdr! record value)
+                  (set-mcdr! subtable
+                             (mcons (mcons key-2 value)
+                                    (mcdr subtable)))))
+            (set-mcdr! local-table
+                       (mcons (list key-1
+                                    (mcons key-2 value))
+                              (mcdr local-table)))))
+      'OK)
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
             ((eq? m 'insert-proc) insert!)
             (else (error "Unknown operation -- TABLE" m))))
     dispatch))
 
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
+(define (make-bst-table)
+  (let ((local-table (mlist '*table*)))
+    (define (lookup key)
+      (let ((records (mcdr local-table)))
+        (define (inner-lookup key records)
+          #| Of all the standard Scheme values, only #f counts as false in conditional expressions.
+Except for #f, all standard Scheme values, including #t, pairs, the empty list, symbols,
+numbers, strings, vectors, and procedures, count as true. |#
+          (if (not (null? records)) ; do not use (if records) to expect false action when records ='()
+              (let ((left-child (mcadr records))
+                    (right-child (mcddr records))
+                    (root-key (mcaar records)))
+                (cond ((< key root-key) (inner-lookup key left-child))
+                      ((> key root-key) (inner-lookup key right-child))
+                      (else (mcar records))))
+              #f))
+        (inner-lookup key records)))
+    
+    (define (insert! key value)
+      (let ((records (mcdr local-table)))
+        (define (inner-insert! records key value)
+          (let ((left-child (mcadr records))
+                (right-child (mcddr records))
+                (root-key (mcaar records)))
+            (cond ((< key root-key)
+                   (if (null? left-child)
+                       (set-mcar! (mcdr records) (mcons (mcons key value) (mcons '() '())))
+                       (inner-insert! left-child key value)))
+                  ((> key root-key)
+                   (if (null? right-child)
+                       (set-mcdr! (mcdr records) (mcons (mcons key value) (mcons '() '())))
+                       (inner-insert! right-child key value)))
+                  (else
+                   (set-mcdr! (mcar records) value)))))
+        (if (null? (mcdr local-table))
+            (set-mcdr! local-table (mcons (mcons key value) (mcons '() '())))
+            (inner-insert! records key value))))
+    
+    (define (print)
+      local-table)
+    
+    (define (dispatch m)
+      (cond ((equal? m 'insert!) insert!)
+            ((equal? m 'look-up) lookup)
+            ((equal? m 'print) print)
+            (else (error "Unknown command!" m))))
+    dispatch))
+
+(define t1 (make-bst-table))
+(define get (t1 'look-up))
+(define put (t1 'insert!))
+
+(put 7 'a)
+(put 9 'b)
+(put 5 'c)
+(put 3 'd)
+(put 8 'e)
+((t1 'print))
+(get 4)
