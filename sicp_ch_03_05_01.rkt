08bb1d02fc98ab463aed2c85e05edffc96781066
songlongjun <songlongjun@pset.suntec.net>
2016-05-26 20:42:42 +0800

    implement excersize by using built-in stream

--- sicp_ch_03_05_01.rkt
+++ sicp_ch_03_05_01.rkt
@@ -1,28 +1,20 @@
 #lang racket
 (require racket/stream)
 
-(define (stream-cons a b)
-  (cons a (delay b)))
-
-(define (stream-car stream)
-  (car stream))
-(define (stream-cdr stream)
-  (force (cdr stream)))
-
 (define (stream-enumerate-interval low high)
   (if (> low high)
-      the-empty-stream
-      (cons-stream
+      empty-stream
+      (stream-cons
        low
-       (stream-enumerate-interval (+low 1) high))))
+       (stream-enumerate-interval (+ low 1) high))))
 
-(define (stream-filter pred stream)
-  (cond ((stream-null? stream) the-empty-stream)
-        ((pred (stream-car stream))
-         (cons (stream-car stream)
+(define (filter-stream pred stream)
+  (cond ((stream-empty? stream) empty-stream)
+        ((pred (stream-first stream))
+         (cons (stream-rest stream)
                (stream-filter pred
-                              (stream-cdr stream))))
-        (else (stream-filter pred (stream-cdr stream)))))
+                              (stream-rest stream))))
+        (else (filter-stream pred (stream-rest stream)))))
 
 (define (memo-proc proc)
   (let ((already-run? false) (result false))
@@ -38,3 +30,34 @@
 
 (define (force delayed-object)
   (delayed-object))
+
+(define (map-stream proc . argstreams)
+  (if (stream-empty? (stream-first argstreams))
+      empty-stream
+      (stream-cons
+       (apply proc (map stream-first argstreams))
+       (apply map-stream
+              (cons proc (map stream-rest argstreams))))))
+
+(define (show x)
+  (displayln x)
+  x)
+
+(define x (map-stream show (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define (display-stream s)
+  (stream-for-each displayln s))
+
+(define seq (map-stream accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+  seq))
+
+(stream-ref y 7)
+(display-stream z)
