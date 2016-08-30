69e42ed10866357f557320fc761b0d31a9d6a08a
songlongjun <songlongjun@pset.suntec.net>
2016-08-30 21:06:28 +0800

    finish reading 4.3.3

--- sicp_ch_04_03_03.rkt
+++ sicp_ch_04_03_03.rkt
@@ -1,10 +1,64 @@
 #lang racket
+(require "sicp_ch_04_01_03.rkt")
+(require "sicp_ch_04_01_01.rkt")
+(require "sicp_ch_04_01_04.rkt")
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
 
 (define (amb? exp) (tagged-list? exp 'amb))
+(define (ramb? exp) (tagged-list? exp 'ramb))
 (define (amb-choices exp) (cdr exp))
+
+(define (require? exp) (tagged-list? exp 'require))
+(define (require-predicate exp) (cadr exp))
+
+(define (if-fail? exp) (tagged-list? exp 'if-fail))
+(define (if-fail-first exp) (cadr exp))
+(define (if-fail-second exp) (caddr exp))
 
 (define (ambeval exp env succeed fail)
   ((analyze exp) env succeed fail))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((amb? exp) (analyze-amb exp))
+        ((ramb? exp) (analyze-ramb exp))
+        ((variable? exp) (analyze-variable exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((if? exp) (analyze-if exp))
+        ((begin? exp) (analyze-sequence exp))
+        ((definition? exp) (analyze-definition exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((application? exp) (analyze-application exp))
+        ((require? exp) (analyze-require exp))
+        ((if-fail? exp) (analyze-if-fail exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(define the-global-environment (setup-environment))
+
+(define (analyze-require exp)
+  (let ((pproc (analyze (require-predicate exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (pred-value fail2)
+               (if (not (true? pred-value))
+                   (fail2)
+                   (succeed 'OK fail2)))
+             fail))))
 
 (define (analyze-self-evaluating exp)
   (lambda (env succeed fail)
@@ -34,11 +88,21 @@
     (lambda (env succeed fail)
       (pproc env
              (lambda (pred-value fail2)
-               (if (ture? pred-value)
+               (if (true? pred-value)
                    (cproc env succeed fail2)
                    (aproc env succeed fail2)))
              fail))))
 
+(define (analyze-if-fail exp)
+  (let ((fproc (analyze (if-fail-first exp)))
+        (sproc (analyze (if-fail-second exp))))
+    (lambda (env succeed fail)
+      (fproc env
+             (lambda (value fail2)
+               (succeed value fail2))
+             (lambda ()
+               (sproc env succeed fail))))))
+       
 (define (analyze-sequence exps)
   (define (sequentially a b)
     (lambda (env succeed fail)
@@ -47,12 +111,12 @@
            (b env succeed fail2))
          fail)))
   (define (loop first-proc rest-procs)
-    (if (nul? rest-procs)
+    (if (null? rest-procs)
         first-proc
         (loop (sequentially first-proc (car rest-procs))
               (cdr rest-procs))))
   (let ((procs (map analyze exps)))
-    (if (null? procs)
+    (when (null? procs)
         (error "Empty sequence -- ANALYZE"))
     (loop (car procs) (cdr procs))))
 
@@ -62,7 +126,7 @@
     (lambda (env succeed fail)
       (vproc env
              (lambda (val fail2)
-               (define-variable! var var1 env)
+               (define-variable! var val env)
                (succeed 'OK fail2))
              fail))))
 
@@ -74,7 +138,7 @@
              (lambda (val fail2)
                (let ((old-value
                       (lookup-variable-value var env)))
-                 (set-variable-value! var var1 env)
+                 (set-variable-value! var val env)
                  (succeed 'OK
                           (lambda ()
                             (set-variable-value! var
@@ -82,3 +146,102 @@
                                                  env)
                             (fail2)))))
              fail))))
+
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env succeed fail)
+      (fproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                            proc args succeed fail3))
+                         fail2))
+             fail))))
+
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+      (succeed '() fail)
+      ((car aprocs) env
+                    (lambda (arg fail2)
+                      (get-args (cdr aprocs)
+                                env
+                                ;; success continuation for recursive
+                                ;; call to get-args
+                                (lambda (args fail3)
+                                  (succeed (cons arg args)
+                                           fail3))
+                                fail2))
+                    fail)))
+
+(define (execute-application proc args succeed fail)
+  (cond ((primitive-procedure? proc)
+         (succeed (apply-primitive-procedure proc args)
+                  fail))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))
+          succeed
+          fail))
+        (else
+         (error
+          "Unknown procedure type -- EXECUTE-APPLICATION"
+          proc))))
+
+(define (analyze-amb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+            ((car choices) env
+                           succeed
+                           (lambda ()
+                             (try-next (cdr choices))))))
+      (try-next cprocs))))
+
+(define (analyze-ramb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (let ((len (length cprocs)))
+      (lambda (env succeed fail)
+        (define (try-random choices)
+          (if (null? choices)
+              (fail)
+              (let ((item (list-ref cprocs (random len))))
+                (item env succeed (lambda () (try-random (remove item choices)))))))
+        (try-random cprocs)))))
+
+(define input-prompt ";;; Amb-Eval input: ")
+(define output-prompt ";;; Amb-Eval value: ")
+
+(define (driver-loop)
+  (define (internal-loop try-again)
+    (prompt-for-input input-prompt)
+    (let ((input (read)))
+      (if (eq? input 'try-again)
+          (try-again)
+          (begin
+            (newline)
+            (display ";;; Starting a new problem ")
+            (ambeval input
+                     the-global-environment
+                     (lambda (val next-alternative)
+                       (announce-output output-prompt)
+                       (user-print val)
+                       (internal-loop next-alternative))
+                     (lambda ()
+                       (announce-output
+                        ";;; There are no more values of")
+                       (user-print input)
+                       (driver-loop)))))))
+  (internal-loop
+   (lambda ()
+     (newline)
+     (display ";;; There is no current problem")
+     (driver-loop))))
+
+(driver-loop)
